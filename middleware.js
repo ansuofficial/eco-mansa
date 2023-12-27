80672d9691516c4667c838b60a2451ab959ac18a
Ansuofficial <badjieansu165@gmail.com>
2023-12-27 21:57:14 +0000

    first commit

---
+++ middleware.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.json())
+// app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', '/index.html'))
+})
+
+
+
+
+
+
+
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./middleware')
+
+app.listen(3030, ()=>{
+    console.log('App is listening on port 3030')
+})
